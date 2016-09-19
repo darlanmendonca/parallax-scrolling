378d68c64a6f65fe0d64cf1ff45d8d9f1a79418f
Darlan Mendonça <darlanmendonca@gmail.com>
2016-09-19 11:36:35 -0300

    refactor script

--- sources/scripts/parallax-scrolling.js
+++ sources/scripts/parallax-scrolling.js
@@ -1,26 +1,22 @@
 'use strict';
 
-let parallax = document.querySelector('.parallax-scrolling');
-let layers = Array.from(document.querySelectorAll('.parallax-scrolling .layer-background'));
-if (layers.length) {
-  setZIndexLayer();
-}
+let parallax;
+let layers;
+let layerVisible;
 
-function getOffsetTop(layer) {
-  return layer.offsetTop;
-}
+parallaxScrolling();
 
-function closestNumber(prev, curr) {
-  return Math.abs(curr - parallax.scrollTop) < Math.abs(prev - parallax.scrollTop)
-    ? curr
-    : prev;
-}
+function parallaxScrolling(element) {
+  parallax = element || parallax || document.querySelector('.parallax-scrolling');
+  layers = layers || Array.from(parallax.querySelectorAll('.layer-background'));
 
-function setZIndexLayer() {
-  let layersOffsetTop = layers.map(getOffsetTop);
-  let layerVisible = layers[layersOffsetTop.indexOf(layersOffsetTop.reduce(closestNumber))];
+  if (layers.length) {
+    let offsetTop = layers.map(layer => layer.offsetTop);
+    layerVisible = layers[offsetTop.indexOf(offsetTop.reduce(closestNumber))];
 
-  layers.map(setZIndex);
+    layers.map(setZIndex);
+    requestAnimationFrame(parallaxScrolling);
+  }
 
   function setZIndex(layer) {
     let isLayerVisible = layer === layerVisible;
@@ -28,5 +24,12 @@ function setZIndexLayer() {
       ? 5
       : 0;
   }
-  requestAnimationFrame(setZIndexLayer);
+
+  function closestNumber(prev, curr) {
+    return Math.abs(curr - parallax.scrollTop) < Math.abs(prev - parallax.scrollTop)
+      ? curr
+      : prev;
+  }
 }
+
+// module.exports = ParallaxScrolling;
